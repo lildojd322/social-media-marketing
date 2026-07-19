c0a960aaf419af9164e1b0a424e7b7bef792ed03
Ivan Filippenko <sevcenkoivan395@gmail.com>
2026-07-19 17:42:36 +0300

    creating the Services section

--- src/app/components/Services/Services.jsx
+++ src/app/components/Services/Services.jsx
@@ -1,7 +1,54 @@
+import styles from './Services.module.scss'
+
 const Services = () => {
     return (
-        <div>
-            
+        <div className={styles.main}>
+            <header className={styles.header}>
+                <h2 className={styles.h2}>//SERVIZI</h2>
+                <h1 className={styles.h1}>
+                    Il content che cattura l'attenzione, la strategia che porta conversioni.
+                </h1>
+            </header>
+            <ul className={styles.servicesList}>
+                <li className={styles.servicesItem}>
+                    <span className={styles.servicesCounter}>/01</span>
+                    <h1 className={styles.servicesName}>Progettazione dell'esperienza utente</h1>
+                    <h2 className={styles.servicesSlogan}>Eleva il tuo stile</h2>
+                    <p className={styles.servicesDescription}>Trasforma il tuo sito web in una piattaforma intuitiva e visivamente accattivante grazie alla nostra competenza nel UX design.</p>
+                </li>
+                <li className={styles.servicesItem}>
+                    <span className={styles.servicesCounter}>/02</span>
+                    <h1 className={styles.servicesName}>Progettazione dell'esperienza utente</h1>
+                    <h2 className={styles.servicesSlogan}>Eleva il tuo stile</h2>
+                    <p className={styles.servicesDescription}>Trasforma il tuo sito web in una piattaforma intuitiva e visivamente accattivante grazie alla nostra competenza nel UX design.</p>
+                </li>
+                <li className={styles.servicesItem}>
+                    <span className={styles.servicesCounter}>/03</span>
+                    <h1 className={styles.servicesName}>Progettazione dell'esperienza utente</h1>
+                    <h2 className={styles.servicesSlogan}>Eleva il tuo stile</h2>
+                    <p className={styles.servicesDescription}>Trasforma il tuo sito web in una piattaforma intuitiva e visivamente accattivante grazie alla nostra competenza nel UX design.</p>
+                </li>
+
+
+                <li className={styles.servicesItem}>
+                    <span className={styles.servicesCounter}>/04</span>
+                    <h1 className={styles.servicesName}>Progettazione dell'esperienza utente</h1>
+                    <h2 className={styles.servicesSlogan}>Eleva il tuo stile</h2>
+                    <p className={styles.servicesDescription}>Trasforma il tuo sito web in una piattaforma intuitiva e visivamente accattivante grazie alla nostra competenza nel UX design.</p>
+                </li>
+                <li className={styles.servicesItem}>
+                    <span className={styles.servicesCounter}>/05</span>
+                    <h1 className={styles.servicesName}>Progettazione dell'esperienza utente</h1>
+                    <h2 className={styles.servicesSlogan}>Eleva il tuo stile</h2>
+                    <p className={styles.servicesDescription}>Trasforma il tuo sito web in una piattaforma intuitiva e visivamente accattivante grazie alla nostra competenza nel UX design.</p>
+                </li>
+                <li className={styles.servicesItem}>
+                    <span className={styles.servicesCounter}>/06</span>
+                    <h1 className={styles.servicesName}>Progettazione dell'esperienza utente</h1>
+                    <h2 className={styles.servicesSlogan}>Eleva il tuo stile</h2>
+                    <p className={styles.servicesDescription}>Trasforma il tuo sito web in una piattaforma intuitiva e visivamente accattivante grazie alla nostra competenza nel UX design.</p>
+                </li>
+            </ul>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Projects from './components/Projects/Projects'
 import Header from './components/header/header'
 import Hero from './components/Hero/Hero'
-
+import Services from './components/Services/Services'
 export default function Home() {
   return (
     <div>
       <Header />
       <Hero />
       <Projects />
+      <Services />
     </div>
   )
 }
