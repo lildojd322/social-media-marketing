20a2ba62307d5e62b0aad9de512f92948e588846
Ivan Filippenko <sevcenkoivan395@gmail.com>
2026-07-21 02:51:21 +0300

    expanding Zod validation error details, increasing the maximum message size, and decreasing the minimum message size

--- src/lib/zod.ts
+++ src/lib/zod.ts
@@ -3,9 +3,9 @@ import { z } from "zod"
 export const bidSchema = z.object({
     email: z.string().email("Formato email non valido"),
     name: z.string()
-        .min(2, 'Il nome è troppo corto')
-        .max(15, 'Il nome è troppo lungo'),
+        .min(2, 'Il nome deve contenere almeno 2 caratteri') 
+        .max(15, 'Il nome non può superare i 15 caratteri'), 
     message: z.string()
-        .min(15, 'Il messaggio è troppo corto')
-        .max(150, 'Il messaggio è troppo lungo'),
+        .min(8, 'Il messaggio deve contenere almeno 8 caratteri') 
+        .max(350, 'Il messaggio non può superare i 350 caratteri'), 
 })
